e4fe33118744956bdbd50778bb9bc2c0c8031cb4
Asanka Wickramasurendra <asankadilshanme@gmail.com>
2024-07-23 14:44:30 +0530

    feat: added custom themes

---
+++ lib/utils/theme/theme.dart
@@ -0,0 +1,26 @@
+import "package:ecommerce/utils/theme/custom_theme/text_theme.dart";
+import "package:flutter/material.dart";
+
+class AppTheme {
+  AppTheme._();
+
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: ATextTheme.lightTextTheme,
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      
+    )
+  );
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.dark,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.black,
+    textTheme: ATextTheme.darkTextTheme,
+  );
+}
