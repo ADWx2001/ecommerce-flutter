209983d3ef2f8f03259ad0c9eb1337964d58008a
Asanka Wickramasurendra <asankadilshanme@gmail.com>
2024-07-24 16:31:12 +0530

    feat: completed constants

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,9 @@
+class AImages{
+  //App Logos
+  static const String darkAppLogo = "assests/logos/t-store-spash-logo-black.png";
+  static const String lightAppLogo = "assests/logos/t-store-spash-logo-white.png";
+
+  //Social Logos
+  static const String google = "assests/logos/google-icon.png";
+  static const String facebook = "assests/logos/facebook-icon.png";
+}
