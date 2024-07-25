e84019a9a031880c1cda7de4168ea65f58069f5f
Asanka Wickramasurendra <asankadilshanme@gmail.com>
2024-07-25 10:10:24 +0530

    feat:small modifications on constants

--- lib/utils/constants/enums.dart
+++ lib/utils/constants/enums.dart
@@ -1,3 +1,5 @@
 enum TextSizes { small, medium, large}
 enum OrderStatus { processing, shipped, delivered}
 enum PaymentMethods { paypal, googlePlay, applePay, visa, masterCard, creditCard, paystack, razorPay, paytm}
+
+// adwx2001
