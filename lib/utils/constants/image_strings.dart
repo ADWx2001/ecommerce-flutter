a4ba4b8253fdc51234d4061c0b639eb322f23719
Asanka Wickramasurendra <asankadilshanme@gmail.com>
2024-08-06 11:34:46 +0530

    feat: onboarding screen 1 created

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -1,11 +1,17 @@
 class AImages{
   //App Logos
-  static const String darkAppLogo = "assests/logos/t-store-spash-logo-black.png";
-  static const String lightAppLogo = "assests/logos/t-store-spash-logo-white.png";
+  static const String darkAppLogo = "assess/logos/t-store-spash-logo-black.png";
+  static const String lightAppLogo = "assets/logos/t-store-spash-logo-white.png";
 
   //Social Logos
-  static const String google = "assests/logos/google-icon.png";
-  static const String facebook = "assests/logos/facebook-icon.png";
+  static const String google = "assets/logos/google-icon.png";
+  static const String facebook = "assets/logos/facebook-icon.png";
+
+  //Onboarding Images
+   static const String onBoardingImage1 = "assets/images/onboarding_images/onboard1.png";
+   static const String onBoardingImage2 = "assets/images/onboarding_images/onboard2.png";
+   static const String onBoardingImage3 = "assets/images/onboarding_images/onboard3.png";
+
 }
 
 // adwx2001
